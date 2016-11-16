125e19f3a288eacc222d7316dcafd4a3cd0bd6dc
Mayoor Rai <mayoorrai@Mayoors-MacBook-Pro.local>
2016-11-15 22:40:51 -0800

    finishedtimeseriestwoassets

--- server/calculator.js
+++ server/calculator.js
@@ -1,15 +1,73 @@
 module.exports = {
     calculatePower: function(body) {
         // extract values array from each asset
-        var asset1Data = body.tags[0].results[0].values;
-        var asset2Data = body.tags[1].results[0].values;
+        var asset1Data = body.tags[0];
+        var asset2Data = body.tags[1];
+        var retValue = {};
+        retValue.aggregatedTimeSeriesData = this.getAggregatePower(asset1Data, asset2Data);
+        retValue.avgPowerData = this.getAvgPower(asset1Data, asset2Data);
+        retValue.maxPowerData = this.getMaxPower(asset1Data, asset2Data);
+        console.log(JSON.stringify(retValue));
+    },
+    getAggregatePower: function(asset1, asset2) {
+        var result = {};
+        result.names = [asset1.name, asset2.name];
+        result.powerVals = [];
+        for (i in asset1.results[0].values) {
+            result.powerVals[i] = asset1.results[0].values[i][1] + asset2.results[0].values[i][1];
+        }
+        return result;
+    },
+    getAvgPower: function(asset1, asset2) {
+        var result = [];
         
-        var powerVal = asset1Data[0];
-        console.log(powerVal[1]);
+        // build json and get avg power values...        
+        var asset1json = {};
+        asset1json.name = asset1.name;
+        var sumPower = 0;
+        for (i in asset1.results[0].values) {
+            sumPower += asset1.results[0].values[i][1];
+        }
+        asset1json.avgPower = sumPower / asset1.results[0].values.length;
+        result.push(asset1json);
+        
+        var asset2json = {};
+        asset2json.name = asset2.name;
+        var sumPower = 0;
+        for (i in asset2.results[0].values) {
+            sumPower += asset2.results[0].values[i][1];
+        }
+        asset2json.avgPower = sumPower / asset2.results[0].values.length;
+        result.push(asset2json);
+        
+        return result;
+    },
+    getMaxPower: function(asset1, asset2) {
+        var result = [];
+        
+        // build json and get max powers...       
+        var asset1json = {};
+        asset1json.name = asset1.name;
+        var currentMaxValue = 0;
+        for (i in asset1.results[0].values) {
+            if (asset1.results[0].values[i][1] > currentMaxValue) {
+                currentMaxValue = asset1.results[0].values[i][1];
+            }
+        }
+        asset1json.maxPower = currentMaxValue;
+        result.push(asset1json);
+        
+        var asset2json = {};
+        asset2json.name = asset2.name;
+        var currentMaxValue = 0;
+        for (i in asset2.results[0].values) {
+            if (asset2.results[0].values[i][1] > currentMaxValue) {
+                currentMaxValue = asset2.results[0].values[i][1];
+            }
+        }
+        asset2json.maxPower = currentMaxValue;
+        result.push(asset2json);
+        
+        return result;
     }
 };
-
-//1. Send aggregated time-series
-//    add power values from two params
-//2. Average power for each
-//3. Max power for each
