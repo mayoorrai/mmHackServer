9e49677a706f15c9871bc0a75ae35efd022f449a
Mayoor Rai <mayoorrai@Mayoors-MacBook-Pro.local>
2016-11-16 11:08:25 -0800

    finalcommit

--- server/calculator.js
+++ server/calculator.js
@@ -1,13 +1,31 @@
 module.exports = {
-    calculatePower: function(body) {
+    calculateValues: function(body) {
         // extract values array from each asset
-        var asset1Data = body.tags[0];
-        var asset2Data = body.tags[1];
+        var asset1TSData = body.tags[0];
+        var asset2TSData = body.tags[1];
+        var asset1OCData = body.tags[2];
+        var asset2OCData = body.tags[3];
+        var asset1FQData = body.tags[4];
+        var asset2FQData = body.tags[5];
+        var asset1MLData = body.tags[6];
+        var asset2MLData = body.tags[7];
+        var asset1RRData = body.tags[8];
+        var asset2RRData = body.tags[9];
+        var asset3OCData = body.tags[10];
+        var asset3PFData = body.tags[11];
+        
         var retValue = {};
-        retValue.aggregatedTimeSeriesData = this.getAggregatePower(asset1Data, asset2Data);
-        retValue.avgPowerData = this.getAvgPower(asset1Data, asset2Data);
-        retValue.maxPowerData = this.getMaxPower(asset1Data, asset2Data);
-        console.log(JSON.stringify(retValue));
+        retValue.aggregatedTimeSeriesData = this.getAggregatePower(asset1TSData, asset2TSData);
+        retValue.avgPowerData = this.getAvgPower(asset1TSData, asset2TSData);
+        retValue.maxPowerData = this.getMaxPower(asset1TSData, asset2TSData);
+        retValue.frequencyData = this.getFrequency(asset1FQData, asset2FQData);
+        retValue.rampRateData = this.getRampRate(asset1RRData, asset2RRData);
+        retValue.minimumLoadData = this.getMinimumLoad(asset1MLData, asset2MLData);
+        retValue.operatingCostData = this.getOperatingCost(asset1OCData, asset2OCData, asset3OCData);
+        retValue.powerFactorData = this.getPowerFactor(asset3PFData);
+        
+        console.log(retValue);
+        return retValue;
     },
     getAggregatePower: function(asset1, asset2) {
         var result = {};
@@ -67,6 +85,81 @@ module.exports = {
         }
         asset2json.maxPower = currentMaxValue;
         result.push(asset2json);
+        
+        return result;
+    },
+    getOperatingCost: function(asset1, asset2, asset3) {
+        var result = [];
+        
+        var asset1json = {};
+        asset1json.name = asset1.name;
+        asset1json.operatingCost = asset1.results[0].values[0][1];
+        result.push(asset1json);
+        
+        var asset2json = {};
+        asset2json.name = asset2.name;
+        asset2json.operatingCost = asset2.results[0].values[0][1];
+        result.push(asset2json);
+        
+        var asset3json = {};
+        asset3json.name = asset3.name;
+        asset3json.operatingCost = asset3.results[0].values[0][1];
+        result.push(asset3json);
+        
+        return result;
+    },
+    getFrequency: function(asset1, asset2) {
+        var result = [];
+        
+        var asset1json = {};
+        asset1json.name = asset1.name;
+        asset1json.frequency = asset1.results[0].values[0][1];
+        result.push(asset1json);
+        
+        var asset2json = {};
+        asset2json.name = asset2.name;
+        asset2json.frequency = asset2.results[0].values[0][1];
+        result.push(asset2json);
+        
+        return result;
+    },
+    getRampRate: function(asset1, asset2) {
+        var result = [];
+        
+        var asset1json = {};
+        asset1json.name = asset1.name;
+        asset1json.rampRate = asset1.results[0].values[0][1];
+        result.push(asset1json);
+        
+        var asset2json = {};
+        asset2json.name = asset2.name;
+        asset2json.rampRate = asset2.results[0].values[0][1];
+        result.push(asset2json);
+        
+        return result;
+    },
+    getMinimumLoad: function(asset1, asset2) {
+        var result = [];
+        
+        var asset1json = {};
+        asset1json.name = asset1.name;
+        asset1json.minimumLoad = asset1.results[0].values[0][1];
+        result.push(asset1json);
+        
+        var asset2json = {};
+        asset2json.name = asset2.name;
+        asset2json.minimumLoad = asset2.results[0].values[0][1];
+        result.push(asset2json);
+        
+        return result;
+    },
+    getPowerFactor: function(asset3) {
+        var result = [];
+        
+        var asset3json = {};
+        asset3json.name = asset3.name;
+        asset3json.powerFactor = asset3.results[0].values[0][1];
+        result.push(asset3json);
         
         return result;
     }
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
 });
 
 // start server
-app.listen(1337);
+app.listen(process.env.PORT || 1337);
 console.log('1337 is the port!');  
 
 
@@ -32,33 +32,54 @@ allAssetsRouter.use(function(req, res, next) {
 // get all assets and return to client
 allAssetsRouter.get('/', function(req, res) {
     console.log("getting all assets...");
-    
-    var jsonBody = {
-                    "start" : "1y-ago",
-                    "tags" : [
-                                {
-                        "name" : [
-                            'Gas Engine 1 - TS'
-                        ]
-                      }
-                    ]
-                  };
-    
-    // build request...
-    var options = requestBuilder.generateRequestForTimeseries(jsonBody);
-   
-// call to db, if successful, return data as json for client
-   request(options, function (error, response, body) {
-       if (!error && response.statusCode == 200) {
-           res.send(body);
+   // get timeseries data for both assets...
+     var timeseries_query_uri     = 'https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/datapoints';
+    var timeseries_zone_id   = '3bc48f70-1519-4b27-a713-cbeb6e6fc60f';
 
-       }
-       else{
-           //console.log(response);
-           res.send(error);
-       }
-   });
-})
+       var json_data =
+           {
+            "start" : "1y-ago",
+            "tags" : [
+                  {
+                    "name" : [
+                        'Gas Engine 1 - TS',
+                        'Gas Engine 1 - OC',
+                        'Gas Engine 1 - FQ',
+                        'Gas Engine 1 - ML',
+                        'Gas Engine 1 - RR'
+                    ]
+                  }
+              ]
+            };
+
+           // options for db call
+           var options = {
+               method: 'POST',
+               url: timeseries_query_uri,
+               headers : {
+                     'Accept': 'application/json, application/x-www-form-urlencoded',
+                     'Authorization': 'Bearer ' + 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkiLCJ0eXAiOiJKV1QifQ.eyJqdGkiOiI2NWVkN2U3NzU4MDc0NDE5OGFmN2U5YTJjZjk1Mzc2ZCIsInN1YiI6ImhhY2thdGhvblVBQSIsInNjb3BlIjpbInRpbWVzZXJpZXMuem9uZXMuM2JjNDhmNzAtMTUxOS00YjI3LWE3MTMtY2JlYjZlNmZjNjBmLnVzZXIiLCJ0aW1lc2VyaWVzLnpvbmVzLjNiYzQ4ZjcwLTE1MTktNGIyNy1hNzEzLWNiZWI2ZTZmYzYwZi5xdWVyeSIsInVhYS5ub25lIiwidGltZXNlcmllcy56b25lcy4zYmM0OGY3MC0xNTE5LTRiMjctYTcxMy1jYmViNmU2ZmM2MGYuaW5nZXN0Il0sImNsaWVudF9pZCI6ImhhY2thdGhvblVBQSIsImNpZCI6ImhhY2thdGhvblVBQSIsImF6cCI6ImhhY2thdGhvblVBQSIsImdyYW50X3R5cGUiOiJjbGllbnRfY3JlZGVudGlhbHMiLCJyZXZfc2lnIjoiOTQxYTMyZTEiLCJpYXQiOjE0NzkzMTI4OTcsImV4cCI6MTQ3OTM1NjA5NywiaXNzIjoiaHR0cHM6Ly9mZmE4MDg3Yy02YmVmLTRmODMtOWZmYi01YmU2N2ZjYWMzODgucHJlZGl4LXVhYS5ydW4uYXdzLXVzdzAyLXByLmljZS5wcmVkaXguaW8vb2F1dGgvdG9rZW4iLCJ6aWQiOiJmZmE4MDg3Yy02YmVmLTRmODMtOWZmYi01YmU2N2ZjYWMzODgiLCJhdWQiOlsidGltZXNlcmllcy56b25lcy4zYmM0OGY3MC0xNTE5LTRiMjctYTcxMy1jYmViNmU2ZmM2MGYiLCJoYWNrYXRob25VQUEiXX0.WT7dc5cMbbouNIcIS48WRAteqeUPGWCpVc_zdB0HiTGXn4gpT0ngpVYXLdQcU7XO5tqGPS8OYdUsc0D-249YYhDz0W4ZDfu-30fdF2vz9AmxmV8TlKL1I1TjHLa0UXFMCaj_4EyhV2w1vYI8P_wJ8dBIy9miB48JmjsobHz9jbrRQQWrB83UiUUTP5Po-yDr8tuk_dBPWFNRxbmAqtjfRXhFS0W0fNmpDE3Q-hjYkAcb0B_Zj8tYJEcn7zthnAmQBKslrBfWU23MQR7QNeu9eeZhMd1szTeK8qaspOcjTpveqI9BmEoTmvaG4YBRtk9VATcmFqa67wUjoIK_OLeP2w',
+                     'Predix-Zone-Id': timeseries_zone_id
+                 },
+               json : json_data
+           };
+
+           // call to db, if successful, return data as json for client
+           request(options, function (error, response, body) {
+               if (!error && response.statusCode == 200) {
+                   //console.log(response);
+
+                   // send to calculator function...
+
+                   res.send(body);
+
+               }
+               else{
+                   console.log(response);
+                   res.send(error);
+               }
+           });
+});
 
 // get instance of router
 var mainRouter = express.Router();
@@ -78,13 +99,16 @@ mainRouter.get('/', function(req, res) {
 });
 
 // users page (http://localhost:1337/admin/users)
-mainRouter.get('/:asset1/:asset2', function(req, res) {
+mainRouter.get('/:asset1/:asset2/:asset3', function(req, res) {
     console.log("getting Block Data...");
 //    TODO: call timeseries...
     var asset1 = req.params['asset1'];
     var asset2 = req.params['asset2'];
+    var asset3 = req.params['asset3'];
     
-    // get timeseries data for both assets...
+    console.log(asset1);
+    console.log(asset2);
+    // get timeseries data for all assets...
      var timeseries_query_uri     = 'https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/datapoints';
     var timeseries_zone_id   = '3bc48f70-1519-4b27-a713-cbeb6e6fc60f';
 
@@ -94,12 +118,23 @@ mainRouter.get('/:asset1/:asset2', function(req, res) {
             "tags" : [
                   {
                     "name" : [
-                        asset1,
-                        asset2
+                        asset1 + ' - TS',
+                        asset2 + ' - TS',
+                        asset1 + ' - OC',
+                        asset2 + ' - OC',
+                        asset1 + ' - FQ',
+                        asset2 + ' - FQ',
+                        asset1 + ' - ML',
+                        asset2 + ' - ML',
+                        asset1 + ' - RR',
+                        asset2 + ' - RR',
+                        asset3 + ' - OC',
+                        asset3 + ' - PF'
                     ]
                   }
               ]
             };
+    console.log(json_data);
 
                // options for db call
                var options = {
@@ -119,9 +154,7 @@ mainRouter.get('/:asset1/:asset2', function(req, res) {
                        //console.log(response);
                        
                        // send to calculator function...
-                       calculator.calculatePower(body);
-                       
-                       res.send(body);
+                       res.send(calculator.calculateValues(body));
 
                    }
                    else{
